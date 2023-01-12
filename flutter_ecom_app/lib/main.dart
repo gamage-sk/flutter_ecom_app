cf160076963d14803681c7ac689a50289cba96b0
Sanjula Kusal Gamage <87776767+SK-Gamage@users.noreply.github.com>
2023-01-12 10:35:57 +0530

    firebase configuration completely done
    firebase configuration completely done

--- flutter_ecom_app/lib/main.dart
+++ flutter_ecom_app/lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecom_app/screen/login_screen.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
